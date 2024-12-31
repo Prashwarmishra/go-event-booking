61b3b8da499089b58a3e39cf3f40e50be46e1c8f
Prashwar <prashwar.mishra@viacom18.com>
2024-12-31 12:16:00 +0530

    init db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-event-booking/db"
 	"go-event-booking/models"
 	"net/http"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
