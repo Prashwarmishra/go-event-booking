8c84c82e3d637253dd334ba081823a6ca318f495
Prashwar <prashwar.mishra@viacom18.com>
2024-12-31 12:55:38 +0530

    insert response into db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"go-event-booking/db"
+	"time"
+)
 
 type Event struct {
-	ID int
+	ID int64
 	Name string `binding:"required"`
 	Description string `binding:"required"`
 	Location string `binding:"required"`
@@ -13,8 +17,29 @@ type Event struct {
 
 var events []Event
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events (name, description, location, datetime, user_id)
+	VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	defer stmt.Close()
+
+	if err != nil {
+		fmt.Println("Error preparing query", err)
+		return err
+	}
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime)
+
+	if err != nil {
+		fmt.Println("Error executing query", err)
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
