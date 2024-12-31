73930db547287e39a049e42f2be83f5cfecf4d30
Prashwar <prashwar.mishra@viacom18.com>
2024-12-31 16:32:38 +0530

    add getEventById api

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-event-booking/db"
 	"go-event-booking/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,17 +14,38 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEventById)
 	server.POST("/events", createEvent)
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{ "message": err })
 		return
 	} 
-	context.JSON(http.StatusOK, gin.H{"message": "request sucessful", "events": events})
+
+	context.JSON(http.StatusOK, gin.H{"message": "request successful", "events": events})
+}
+
+func getEventById(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{ "message": "bad request - invalid event id" })
+		return
+	}
+
+	data, err := models.GetEventById(id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{ "message": "internal server error" })
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "request successful", "event": data})
 }
 
 func createEvent(context *gin.Context) {
@@ -44,5 +66,5 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "request sucessful", "data": data})
+	context.JSON(http.StatusCreated, gin.H{"message": "request successful", "data": data})
 }
